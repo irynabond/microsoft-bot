804d2b1bf886b2f30e4959ddfe0e5de6f1489883
Iryna Bondarenko <irusiabondarenko@gmail.com>
2016-06-11 23:23:07 -0700

    app works in console

--- Traitify/ConsoleApplication3/Program.cs
+++ Traitify/ConsoleApplication3/Program.cs
@@ -10,35 +10,38 @@ namespace ConsoleApplication3
 {
     class Program
     {
-        //public class SlideSelected
-        //{
-        //    public string id { get; set; }
-        //    public bool response { get; set; }
-        //    public int time_taken { get; set; }
-        //}
+        
         static void Main(string[] args)
         {
             Traitify traitify = new Traitify("https://api.traitify.com", "bt5bo444mk38efl4crbat39scv", "pl17doqlrachpa7jg5oqe3ja7p", "v1");
 
             List<Deck> decks = traitify.GetDecks();
-            Assessment assess = traitify.CreateAssesment(decks[0].id);
-            //Assessment assess = traitify.CreateAssesment("career-deck");
+            foreach (Deck deck in decks)
+            {
+                Console.WriteLine(deck.id);
+            }
+            Console.WriteLine("Please, choose one test");
+            string testName = Console.ReadLine();
+
+            Assessment assess = traitify.CreateAssesment(testName);
             string assessment_id = assess.id;
             Assessment assessment = traitify.GetAssessment(assessment_id);
             List<Slide> slides = traitify.GetSlides(assessment_id);
-            //List<object>SlidesList = new List<object>();
 
+            Console.WriteLine("Please give the asnswer true or false to the following statements");
             foreach (Slide slide in slides)
             {
+                Console.WriteLine(slide.caption);
                 slide.time_taken = 600;
-                slide.response = true;
-                              
+                slide.response = Convert.ToBoolean(Console.ReadLine());                          
             }
-            //SlidesList.Add(new SlideSelected() { id = slide.id, response = true, time_taken = 600 });
+
             traitify.SetSlideBulkUpdate(assessment_id, slides);
             AssessmentPersonalityTypes types = traitify.GetPersonalityTypes(assessment_id);
             List<AssessmentPersonalityType> personalityTypes = types.personality_types;
-            Console.WriteLine("Type: " + personalityTypes[0].personality_type.name);
+            Console.WriteLine("Your type of personality is: " + personalityTypes[0].personality_type.name);
+            Console.WriteLine(personalityTypes[0].personality_type.description);
+            Console.WriteLine(personalityTypes[0].personality_type.badge.image_medium);
         }
     }
 }
