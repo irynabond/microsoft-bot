9fbebcdd462febf23048592feabd906f86da5ee2
Iryna Bondarenko <irusiabondarenko@gmail.com>
2016-06-23 15:19:04 -0700

    possible to stop conversation or start a new one

--- Bot Application2/Bot Application2/Controllers/MessagesController.cs
+++ Bot Application2/Bot Application2/Controllers/MessagesController.cs
@@ -26,7 +26,8 @@ namespace Bot_Application2
         bool response;
         int index = 0;
         string test_name;
-        List<Slide> slideCollection;
+        List<Slide> slideCollection=null;
+
         public async Task StartAsync(IDialogContext context)
         {
             context.Wait(MessageReceivedAsync);
@@ -34,59 +35,74 @@ namespace Bot_Application2
 
         public virtual async Task MessageReceivedAsync(IDialogContext context, IAwaitable<Message> argument)
         {
-
-            if (showDecks == false)
+            var messageStop = await argument;
+            if (messageStop.Text == "no" || messageStop.Text == "No"||messageStop.Text == "stop")
             {
-                showDecks = true;
-                TestType names = new TestType();
-                var listOfDecks = string.Join(", ", names.GetNames().ToArray());
-                await context.PostAsync("Hello! Please, choose a topic: " + listOfDecks);
+                await context.PostAsync("Ok! Thank you. See you next time.");
                 context.Wait(MessageReceivedAsync);
-
             }
             else
             {
-                if (slideStarted == false)
+                if (showDecks == false)
                 {
-                    slideStarted = true;
-                    var message = await argument;
-                    TestType slides = new TestType();
-                    test_name = message.Text;
-                    slideCollection = slides.GetSlides(test_name);
-                    await context.PostAsync("Your test is ready! For the following questions type 1 if your answer is yes, type 2 if your answer is no. In order to start type Ready or Start.");
+                    showDecks = true;
+                    TestType names = new TestType();
+                    var listOfDecks = string.Join(", ", names.GetNames().ToArray());
+                    await context.PostAsync("Please, choose the test: " + listOfDecks);
                     context.Wait(MessageReceivedAsync);
-                
-                } else
+
+                }
+                else
                 {
-                    if (index==0)
+                    if (slideStarted == false)
                     {
-                        string name = slideCollection[index].caption;
-                        index++;
-                        await context.PostAsync("![" + name + "](" + slideCollection[index-1].image_desktop + ")");
+                        slideStarted = true;
+                        var message = await argument;
+                        TestType slides = new TestType();
+                        test_name = message.Text;
+                        slideCollection = slides.GetSlides(test_name);
+                        await context.PostAsync("Your test is ready! For the following questions type 1 if your answer is yes, type 2 if your answer is no. In order to start type Ready or Start.");
                         context.Wait(MessageReceivedAsync);
-                    } else
+
+                    }
+                    else
                     {
-                        if (index < slideCollection.Count)
+                        if (index == 0)
                         {
-                            var message = await argument;
-                            response = (message.Text == "1") ? true : false;
-                            string name = slideCollection[index].caption;                        
-                            slideCollection[index - 1].response = response;
-                            index++;                            
-                            await context.PostAsync("![" + name + "](" + slideCollection[index-1].image_desktop + ")");
-                            context.Wait(MessageReceivedAsync);
-                        } else if (index==slideCollection.Count)
-                        {
-                            var message = await argument;
-                            response = (message.Text == "2") ? false : true;
-                            slideCollection[index - 1].response = response;
-                            TestType result = new TestType();
-                            string personality_type = result.Result("test", slideCollection);
-                            await context.PostAsync(personality_type);
+                            string name = slideCollection[index].caption;
+                            index++;
+                            await context.PostAsync("![" + name + "](" + slideCollection[index - 1].image_desktop + ")");
                             context.Wait(MessageReceivedAsync);
                         }
-                    }                                            
-                }                 
+                        else
+                        {
+                            if (index < slideCollection.Count)
+                            {
+                                var message = await argument;
+                                response = (message.Text == "1") ? true : false;
+                                string name = slideCollection[index].caption;
+                                slideCollection[index - 1].response = response;
+                                index++;
+                                await context.PostAsync("![" + name + "](" + slideCollection[index - 1].image_desktop + ")");
+                                context.Wait(MessageReceivedAsync);
+                            }
+                            else if (index == slideCollection.Count)
+                            {
+                                var message = await argument;
+                                response = (message.Text == "2") ? false : true;
+                                slideCollection[index - 1].response = response;
+                                TestType result = new TestType();
+                                string personality_type = result.Result("test", slideCollection);
+                                showDecks = false;
+                                slideStarted = false;
+                                index = 0;
+                                await context.PostAsync(personality_type);
+                                await context.PostAsync("Would you like to take one more test?");
+                                context.Wait(MessageReceivedAsync);
+                            }
+                        }
+                    }
+                }
             }
         }
     }
